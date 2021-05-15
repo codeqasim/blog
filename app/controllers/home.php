4223d08b699b7a91a8fdc3221bf0a5416c18f235
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-15 21:31:15 +0500

    db update and added flush feature

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -24,6 +24,42 @@ $body = views."newsletters.php";
 include template;
 });
 
+// main homepage
+$router->get('install', function() {
+
+// Do not modify anything under this line :)
+    class db {
+    var $dbhost;
+    var $dbuser;
+    var $dbpassword;
+    var $dbname;
+    var $query;
+        function connect() {
+            $this->db = new mysqli($this->dbhost, $this->dbuser, $this->dbpassword, $this->dbname);
+            mysqli_set_charset($this->db,"utf8");
+        }
+        function __construct() {
+            $this->dbhost = server;
+            $this->dbuser = username;
+            $this->dbpassword = password;
+            $this->dbname = dbname;
+        }
+    }
+    $con = new db;
+    $con->connect();
+   // $res = $con->db->query("SELECT * FROM pt_accounts");
+     $sql= file_get_contents('db.sql');
+     foreach (explode(";\n", $sql) as $sql)
+       {
+         $sql = trim($sql);
+           if($sql)
+               {
+                $con->db->query($sql);
+               }
+      }
+      echo "<p>Installation Completed</p>";
+});
+
 // post page
 $router->get('(.*)', function() {
 include "app/db.php";
--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2021 at 06:04 PM
+-- Generation Time: May 15, 2021 at 06:30 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `blog`
+-- Database: `booknow_blog`
 --
 
 -- --------------------------------------------------------
