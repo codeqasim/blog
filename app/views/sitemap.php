01e11e967f11977eb5bdf023504fd70a818e51a2
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-16 03:32:51 +0500

    Update sitemap.php

--- app/views/sitemap.php
+++ app/views/sitemap.php
@@ -3,7 +3,7 @@
 <lastmod>2021-05-08T20:00:00.086Z</lastmod>
 </sitemap>
 <sitemap>
-<loc>https://technewspakistan.com/sitemap-posts.xml</loc>
+<loc><?=root?>sitemap-posts.xml</loc>
 <lastmod>2021-05-12T19:37:10.406Z</lastmod>
 </sitemap>
 <sitemap>
