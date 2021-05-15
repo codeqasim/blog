6c857c3506d8122ce64ff5ec790dea07edcee5cb
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-16 03:33:38 +0500

    Update sitemap.php

--- app/views/sitemap.php
+++ app/views/sitemap.php
@@ -1,5 +1,6 @@
-﻿<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap>
-<loc>https://technewspakistan.com/sitemap-pages.xml</loc>
+﻿<?xml version="1.0" encoding="UTF-8"?><?xml-stylesheet type="text/xsl" href="<?=root?>app/views/sitemap.xsl"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap>
+<loc><?=root?>sitemap-pages.xml</loc>
 <lastmod>2021-05-08T20:00:00.086Z</lastmod>
 </sitemap>
 <sitemap>
@@ -7,11 +8,11 @@
 <lastmod>2021-05-12T19:37:10.406Z</lastmod>
 </sitemap>
 <sitemap>
-<loc>https://technewspakistan.com/sitemap-authors.xml</loc>
+<loc><?=root?>sitemap-authors.xml</loc>
 <lastmod>2021-05-14T21:47:43.720Z</lastmod>
 </sitemap>
 <sitemap>
-<loc>https://technewspakistan.com/sitemap-tags.xml</loc>
+<loc><?=root?>sitemap-tags.xml</loc>
 <lastmod>2021-05-12T19:34:56.000Z</lastmod>
 </sitemap>
 </sitemapindex>
