c8792c4a9ea969ef79c3241afba5705e91fca3ed
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-16 01:19:08 +0500

    meta bug fixes

--- app/views/front/header.php
+++ app/views/front/header.php
@@ -12,6 +12,9 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet">
 
+<link rel="canonical" href="<?=$meta_url?>" />
+<meta name="referrer" content="no-referrer-when-downgrade" />
+
 <!-- meta open graph -->
 <meta property="og:site_name" content="<?=$app->app_name?>" />
 <meta property="og:type" content="article" />
