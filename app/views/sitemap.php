3b2e04c36a41d4e72a09d18b9245b01f828eb0bc
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-16 02:42:23 +0500

    Update sitemap.php

--- app/views/sitemap.php
+++ app/views/sitemap.php
@@ -1,4 +1,5 @@
-<?xml-stylesheet href="<?=root?>app/views/sitemap.xsl"?>
+<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="<?=root?>app/views/sitemap.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><sitemap>
 <loc>https://technewspakistan.com/sitemap-pages.xml</loc>
 <lastmod>2021-05-08T20:00:00.086Z</lastmod>
