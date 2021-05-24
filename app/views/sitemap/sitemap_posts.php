0856dd59c71b798c96c9d7cfab8aadecfcf56d7b
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-24 16:28:24 +0500

    Update sitemap_posts.php

--- app/views/sitemap/sitemap_posts.php
+++ app/views/sitemap/sitemap_posts.php
@@ -1,18 +1,15 @@
-﻿<?php
-echo'<?xml version="1.0" encoding="UTF-8"?>';
+﻿<?php header('Content-type: application/xml; charset=utf-8') ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>' ?>
+<?php
 echo'
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
 if ($posts->num_rows > 0) { foreach($posts as $post) {
-
 $d = $post['created_at'];
 $date = date('Y-m-d', strtotime($d));
-
 echo '
 <sitemap>
 <loc>'.root.$post['slug'].'</loc>
 <lastmod>'.$date.'</lastmod>
 </sitemap>
 '; }}
-
 echo'</sitemapindex>'; ?>
