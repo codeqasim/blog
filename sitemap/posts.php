453cd4dfe8b67dd286354b34bf9b6a51e7d07589
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:13:09 +0500

    Update posts.php

--- sitemap/posts.php
+++ sitemap/posts.php
@@ -1,18 +1,19 @@
 <?php
 $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST']; $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']); define('root', $root);
-$url = rtrim(root, '/');$url = explode('/', $url);json_encode(array_pop($url));
+$url = rtrim(root, '/');
+$url = explode('/', $url);
+json_encode(array_pop($url));
+$uri = implode('/', $url)."/";
 include "../config.php";
 include "../app/db.php";
 $posts = $mysqli->query("SELECT * FROM posts WHERE status = 1");
 header('Content-type: text/xml');
-echo'<?xml version="1.0" encoding="UTF-8"?>';
-echo'<?xml-stylesheet type="text/xsl" href="'.root.'sitemap.xsl"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-if ($posts->num_rows > 0) { foreach($posts as $post) {
-$d = $post['created_at'];
-$date = date('Y-m-d', strtotime($d));
-echo '<sitemap>
-<loc>'.implode('/', $url)."/".$post['slug'].'</loc>
-<lastmod>'.$date.'</lastmod>
-</sitemap>'; }}
-echo'</sitemapindex>'; ?>
+echo '<?xml version="1.0" encoding="UTF-8" ?>' ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php foreach ($posts as $p){ $d = $p['created_at']; $date = date('Y-m-d', strtotime($d)); ?>
+<url>
+<loc><?=$uri.$p['slug']?></loc>
+<lastmod><?=$date?></lastmod>
+</url>
+<?php } ?>
+</urlset>
