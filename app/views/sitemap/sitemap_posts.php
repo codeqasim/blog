cffe710c976b80d0c5ab15ad2b955f294bbb60a9
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:32:15 +0500

    Update sitemap_posts.php

--- app/views/sitemap/sitemap_posts.php
+++ app/views/sitemap/sitemap_posts.php
@@ -1,9 +1,16 @@
-﻿<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+﻿<?php
+echo'<?xml version="1.0" encoding="UTF-8"?>';
+echo'<?xml-stylesheet type="text/xsl" href="'.root.'app/views/sitemap/sitemap.xsl"?>
+';?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
 <?php if ($posts->num_rows > 0) { foreach($posts as $post) {  $d = $post['created_at']; $date = date('Y-m-d', strtotime($d)); ?>
 <url>
 <loc><?=root.$post['slug']?></loc>
 <lastmod><?=$date?></lastmod>
+<image:image>
+<image:loc><?=root."uploads/posts/".$post['img']?></image:loc>
+<image:caption><?=$post['img']?></image:caption>
+</image:image>
 </url>
 <?php } } ?>
 </urlset>
