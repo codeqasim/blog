7f82b311c3ef3af1994a2f3464c8b1a25de8096f
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:35:57 +0500

    Update sitemap_pages.php

--- app/views/sitemap/sitemap_pages.php
+++ app/views/sitemap/sitemap_pages.php
@@ -1,17 +1,12 @@
 ﻿<?php
 echo'<?xml version="1.0" encoding="UTF-8"?>';
 echo'<?xml-stylesheet type="text/xsl" href="'.root.'app/views/sitemap/sitemap.xsl"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
-$d = $post['created_at'];
-$date = date('Y-m-d', strtotime($d));
-
-if ($pages->num_rows > 0) { foreach($pages as $page) {
-echo '
-<sitemap>
-<loc>'.root.$page['slug'].'</loc>
-<lastmod>'.$date.'</lastmod>
-</sitemap>
-'; }}
-
-echo'</sitemapindex>'; ?>
+';?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<?php if ($pages->num_rows > 0) { foreach($pages as $page) {  $d = $page['created_at']; $date = date('Y-m-d', strtotime($d)); ?>
+<url>
+<loc><?=root.$page['slug']?></loc>
+<lastmod><?=$date?></lastmod>
+</url>
+<?php } } ?>
+</urlset>
