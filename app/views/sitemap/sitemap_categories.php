457c3a402aa9470aa2800f3814388b5cbc60ba55
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:39:18 +0500

    sitemap completed

--- app/views/sitemap/sitemap_categories.php
+++ app/views/sitemap/sitemap_categories.php
@@ -1,18 +1,12 @@
 ﻿<?php
 echo'<?xml version="1.0" encoding="UTF-8"?>';
 echo'<?xml-stylesheet type="text/xsl" href="'.root.'app/views/sitemap/sitemap.xsl"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
-
-if ($categories->num_rows > 0) { foreach($categories as $category) {
-
-$d = $post['created_at'];
-$date = date('Y-m-d', strtotime($d));
-
-echo '
-<sitemap>
-<loc>'.root.$category['slug'].'</loc>
-<lastmod>'.$date.'</lastmod>
-</sitemap>
-'; }}
-
-echo'</sitemapindex>'; ?>
+';?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+<?php if ($categories->num_rows > 0) { foreach($categories as $category) {  $d = $category['created_at']; $date = date('Y-m-d', strtotime($d)); ?>
+<url>
+<loc><?=root.$category['slug']?></loc>
+<lastmod><?=$date?></lastmod>
+</url>
+<?php } } ?>
+</urlset>
