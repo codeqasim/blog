ab6b8f326f4d0d019c6dcb3f9946b43756d5eb52
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:20:03 +0500

    sitemap

--- app/views/sitemap/sitemap.php
+++ app/views/sitemap/sitemap.php
@@ -25,3 +25,13 @@ $date_time = $date .' '. $time;
 </sitemap>
 -->
 </sitemapindex>'; ?>
+
+<?php echo '<?xml version="1.0" encoding="UTF-8" ?>'?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php if ($posts->num_rows > 0) { foreach($posts as $post) {  $d = $post['created_at']; $date = date('Y-m-d', strtotime($d)); ?>
+<url>
+<loc><?=root.$post['slug']?></loc>
+<lastmod><?=$date?></lastmod>
+</url>
+<?php } } ?>
+</urlset>
