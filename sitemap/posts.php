dfaa29af6fb588c870db87af748b6b6574f5e97f
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-25 04:00:02 +0500

    site

--- sitemap/posts.php
+++ sitemap/posts.php
@@ -1,24 +1,18 @@
 <?php
 $root=(isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST']; $root.= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']); define('root', $root);
-//then remove character from right
-$url = rtrim(root, '/');
-// then explode
-$url = explode('/', $url);
-// remove the last element and return an array
-json_encode(array_pop($url));
-// implode again into string
-
+$url = rtrim(root, '/');$url = explode('/', $url);json_encode(array_pop($url));
 include "../config.php";
 include "../app/db.php";
 $posts = $mysqli->query("SELECT * FROM posts WHERE status = 1");
 header('Content-type: text/xml');
-echo '<?xml version="1.0" encoding="UTF-8" ?>' ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<url>
-<?php foreach ($posts as $p){ $d = $p['created_at']; $date = date('Y-m-d', strtotime($d));
-?>
-<loc><?=implode('/', $url)."/".$p['slug']?></loc>
-<lastmod><?=$date?></lastmod>
-<?php } ?>
-</url>
-</urlset>
+echo'<?xml version="1.0" encoding="UTF-8"?>';
+echo'<?xml-stylesheet type="text/xsl" href="'.root.'sitemap.xsl"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+if ($posts->num_rows > 0) { foreach($posts as $post) {
+$d = $post['created_at'];
+$date = date('Y-m-d', strtotime($d));
+echo '<sitemap>
+<loc>'.root.$post['slug'].'</loc>
+<lastmod>'.$date.'</lastmod>
+</sitemap>'; }}
+echo'</sitemapindex>'; ?>
