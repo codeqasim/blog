8c502f85f0397b4bf6ae8cd9d0f8bc995748d0ef
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-24 11:40:19 +0500

    Update db.php

--- blog/app/db.php
+++ blog/app/db.php
@@ -4,9 +4,8 @@
 $mysqli = new mysqli(server, username, password, dbname);
 
 // database connection condition
-if ($mysqli -> connect_errno) {
-header("Location: install");
-//echo "Failed to connect to MySQL: " . $mysqli -> connect_error; exit();
+if ($mysqli -> connect_errno) { // header("Location: install");
+echo "Failed to connect to MySQL: " . $mysqli -> connect_error; exit();
 } else { }
 
 // main app credentials, can be called by $app->FIELD_NAME
