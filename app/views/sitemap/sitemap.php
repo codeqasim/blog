9d3d38e763ff80fd554bc5ef9b911e4d282563bc
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-24 12:39:10 +0500

    Update sitemap.php

--- app/views/sitemap/sitemap.php
+++ app/views/sitemap/sitemap.php
@@ -6,22 +6,22 @@ $date_time = $date .' '. $time;
 <?php echo'<?xml-stylesheet type="text/xsl" href="'.root.'app/views/sitemap/sitemap.xsl"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <sitemap><loc>'.root.'sitemap-pages.xml</loc>
-<lastmod>'.$date_time.'</lastmod></sitemap>
+<lastmod>'.$date.'</lastmod></sitemap>
 <sitemap><loc>'.root.'sitemap-posts.xml</loc>
-<lastmod>'.$date_time.'</lastmod>
+<lastmod>'.$date.'</lastmod>
 </sitemap>
 <sitemap>
 <loc>'.root.'sitemap-authors.xml</loc>
-<lastmod>'.$date_time.'</lastmod>
+<lastmod>'.$date.'</lastmod>
 </sitemap>
 <sitemap>
 <loc>'.root.'sitemap-categories.xml</loc>
-<lastmod>'.$date_time.'</lastmod>
+<lastmod>'.$date.'</lastmod>
 </sitemap>
 <!--
 <sitemap>
 <loc>'.root.'sitemap-tags.xml</loc>
-<lastmod>'.$date_time.'</lastmod>
+<lastmod>'.$date.'</lastmod>
 </sitemap>
 -->
 </sitemapindex>'; ?>
