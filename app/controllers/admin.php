a794d6a7b3e01d63107edfd652d78c48af1a4b40
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-26 04:41:45 +0500

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -310,11 +310,11 @@ if(move_uploaded_file($temp_name, $target_path)) {
 //
 }else{ exit("Error While uploading image on the server"); } }
 
-if (!empty($_POST['img'])) {
+if (isset($_POST['img'])) {
 $image=$_POST['img'];
 $img_emp = explode('/', $image);
 $img = end($img_emp);
-}
+} else { $img =""; }
 
 // array to sting for keywords
 if (isset($_POST['keywords'])) { $keywords = implode (", ", $_POST['keywords']); } else { $keywords = ""; }
