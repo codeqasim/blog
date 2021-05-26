995d20b46b8f058b53b07af56404cdff7892e65e
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-26 05:08:09 +0500

    Update post.php

--- app/views/admin/post.php
+++ app/views/admin/post.php
@@ -154,7 +154,6 @@ $('.status option[value=<?=$status?>]').attr('selected','selected');
 
 <script>
 ClassicEditor.create(document.querySelector(".editor"), {
-        config.entities = false;
         toolbar: {
             items: [
                 "heading",
