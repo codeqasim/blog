9db52c8dcd494f24fd093bf12ad0cb41690ede10
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-26 09:09:05 +0500

    Update categories.php

--- app/views/front/categories.php
+++ app/views/front/categories.php
@@ -4,9 +4,9 @@
 </section>
 
 <?php
-$cat = $mysqli->query("SELECT * FROM `categories` WHERE `slug` LIKE '".$url_end."' LIMIT 50");
+$cat = $mysqli->query("SELECT * FROM `categories` WHERE `slug` LIKE '".$url_end."'");
 foreach ($cat as $c ){ $cat_id = $c['id']; }
-$posts = $mysqli->query("SELECT * FROM posts WHERE status = 1 AND category_id LIKE '".$cat_id."' ORDER BY id DESC LIMIT 100");
+$posts = $mysqli->query("SELECT * FROM posts WHERE status = 1 AND category_id LIKE '".$cat_id."' ORDER BY id DESC LIMIT 50");
 ?>
 
 <div class="categories">
