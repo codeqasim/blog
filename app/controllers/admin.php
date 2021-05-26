a272c5989b761fb7c8ef8348336d6eee38f95c02
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-26 05:17:06 +0500

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -322,7 +322,7 @@ if (isset($_POST['keywords'])) { $keywords = implode (", ", $_POST['keywords']);
 // check type of post to run mysql query
 if ($_POST['post_type'] == "update") {
 
-$clean = str_replace("'", '&#39;', $_POST['content']);
+$content = str_replace("'", '&#39;', $_POST['content']);
 $status = $_POST['status'];
 $slug = $_POST['slug'];
 $title = $_POST['title'];
