0c05500ed9744434d324018c35e2284ac5fd7b39
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-27 07:39:00 +0500

    Update xcrud.js

--- app/vendor/xcrud/plugins/xcrud.js
+++ app/vendor/xcrud/plugins/xcrud.js
@@ -210,6 +210,53 @@ var Xcrud = {
 				}*/
 			}
 			if (typeof(CKEDITOR) != 'undefined') {
+
+
+
+
+
+				ClassicEditor.create(document.querySelector(".editor-loaded"), {
+toolbar: {
+    items: [
+        "heading",
+        "|",
+        "bold",
+        "italic",
+        "link",
+        "bulletedList",
+        "numberedList",
+        "|",
+        "indent",
+        "outdent",
+        "|",
+        "imageUpload",
+        "blockQuote",
+        "mediaEmbed",
+        "undo",
+        "redo",
+    ],
+},
+language: "en",
+image: {
+    toolbar: ["imageTextAlternative", "imageStyle:full", "imageStyle:side"],
+},
+licenseKey: "",
+})
+.then((editor) => {
+    window.editor = editor;
+})
+.catch((error) => {
+    console.error("Oops, something went wrong!");
+    console.error(
+        "Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:"
+    );
+    console.warn("Build id: ref2goguw78q-8ghiwfe1qu83");
+    console.error(error);
+});
+
+
+
+
 				for (instance in CKEDITOR.instances) {
 					if (jQuery('#' + instance).size()) {
 						CKEDITOR.instances[instance].updateElement();
