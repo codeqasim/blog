452fd581b4faafe415bc007f259b3fe720370022
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-27 07:37:02 +0500

    Update admin.php

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -156,7 +156,7 @@ include('app/vendor/xcrud/xcrud.php');
 $xcrud = Xcrud::get_instance();
 $xcrud->table('pages');
 $xcrud->order_by('id','desc');
-$xcrud->column_class('content', 'texteditor');
+$xcrud->column_class('content', 'editor-loaded');
 $xcrud->columns('id,title,status,created_at');
 $xcrud->unset_title();
 $title ="Pages";
