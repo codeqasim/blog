4ef9a1b344f89363170ddf2b1840a0a7a0ff10e0
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-05-27 07:49:27 +0500

    i

--- app/views/admin/xcrud.php
+++ app/views/admin/xcrud.php
@@ -2,42 +2,9 @@
 <?php echo $xcrud->render(); ?>
 
 <script>
-ClassicEditor.create(document.querySelector(".editor-loaded"), {
-toolbar: {
-    items: [
-        "heading",
-        "|",
-        "bold",
-        "italic",
-        "link",
-        "bulletedList",
-        "numberedList",
-        "|",
-        "indent",
-        "outdent",
-        "|",
-        "imageUpload",
-        "blockQuote",
-        "mediaEmbed",
-        "undo",
-        "redo",
-    ],
-},
-language: "en",
-image: {
-    toolbar: ["imageTextAlternative", "imageStyle:full", "imageStyle:side"],
-},
-licenseKey: "",
-})
-.then((editor) => {
-    window.editor = editor;
-})
-.catch((error) => {
-    console.error("Oops, something went wrong!");
-    console.error(
-        "Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:"
-    );
-    console.warn("Build id: ref2goguw78q-8ghiwfe1qu83");
-    console.error(error);
+// categories slug
+$('.title').keyup(function () {
+title = $(this).val().split(',').slice(0, 1).join(' ').split(' ').join('-').replace('%40', '@').toLowerCase();
+document.getElementsByClassName("slug").value = title;
 });
 </script>
