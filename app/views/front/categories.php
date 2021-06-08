e650c21d44f1b348b0ec07a1f9ae029047c43ef9
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-06-08 20:35:24 +0500

    Update categories.php

--- app/views/front/categories.php
+++ app/views/front/categories.php
@@ -54,3 +54,31 @@ echo $mysqli->query("SELECT * FROM categories WHERE id = ".$catetory."")->fetch_
 </div>
 </div>
 </div>
+
+<script>
+
+function scrollStop (callback, refresh = 66) {
+
+    // Make sure a valid callback was provided
+    if (!callback || typeof callback !== 'function') return;
+
+    // Setup scrolling variable
+    let isScrolling;
+
+    // Listen for scroll events
+    window.addEventListener('scroll', function (event) {
+
+        // Clear our timeout throughout the scroll
+        window.clearTimeout(isScrolling);
+
+        // Set a timeout to run after scrolling ends
+        isScrolling = setTimeout(callback, refresh);
+
+    }, false);
+
+}
+
+scrollStop(function () {
+    console.log('Scrolling has stopped.');
+});
+</script>
