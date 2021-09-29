0603ba109606edfbba40cf77bf87392bbc995acf
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2021-09-29 08:34:27 +0500

    added select 2 on homepage

--- config.php
+++ config.php
@@ -3,5 +3,5 @@
 // database configuration
 define('server','localhost'); // mention your database server name normally its localhost
 define('dbname','blog'); // mention your database name
-define('username','root'); // mention your database username name normally its root
-define('password',''); // mention your database password
+define('username','blog'); // mention your database username name normally its root
+define('password','blog'); // mention your database password
